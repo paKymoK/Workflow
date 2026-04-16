ae3a8d5818249479ede202be376d7506f5f49830
panchew <thaimeo0210@gmail.com>
2026-04-16 23:27:41 +0700

    Refactor Code

---
+++ workflow-front-end/src/router/index.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "../auth/AuthProvider";
+import ProtectedRoute from "../components/ProtectedRoute";
+import Login from "../pages/Login";
+import Portfolio from "../pages/Portfolio";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import TicketDetail from "../pages/TicketDetail";
+import Callback from "../pages/Callback";
+import Settings from "../pages/Settings";
+import WorkflowDetail from "../pages/WorkflowDetail";
+import Diary from "../pages/Diary";
+import AMS from "../pages/AMS";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/ams" element={<AMS />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard/:id" element={<TicketDetail />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/settings/workflow/:id" element={<WorkflowDetail />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
