ea33a2d83c28dec4916009cc1b5ba3f04f177682
panchew <thaimeo0210@gmail.com>
2026-04-26 09:58:46 +0700

    Update postgres init config

--- auth-service/src/main/java/com/takypok/authservice/config/AuthorizationServerConfig.java
+++ auth-service/src/main/java/com/takypok/authservice/config/AuthorizationServerConfig.java
@@ -10,7 +10,6 @@ import java.time.Instant;
 import java.util.UUID;
 import java.util.function.Function;
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
